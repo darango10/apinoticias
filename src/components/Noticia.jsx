f6fb4cc13ecaf149c247ec18bfec94a56cb660b7
Daniel <arangomonsalve.daniel@gmail.com>
2020-05-01 14:49:21 -0500

    Correcion diseño de imagenes

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import styles from './ContenedorNoticia.module.css'
+import PropTypes from "prop-types";
+
 
 const Noticia = ({noticia}) => {
 
     //Exraer los datos d ela noticia
-    const {author, title, description, url, urlToImage, publishedAt, content, source} = noticia
+    const {title, description, url, urlToImage, source} = noticia
 
     const imagen = (urlToImage) ?
-        <div className="card-image">
-            <img src={urlToImage} alt={title}/>
+        <div className={`card-image`}>
+            <img className={`${styles.imagen}`} src={urlToImage} alt={title}/>
             <span className={'card-title'}>{source.name}</span>
         </div>
         : null;
 
     return (
-        <div className={'col s12 m6 l4'}>
-            <div className="card">
+        <div className={`${styles.contenedor} col s12 m6 l4`}>
+            <div className={`${styles.cardLarge} card large`}>
                 {imagen}
-                <div className="card-content">
-                    <h4>{title}</h4>
-                    <p>{description}</p>
+                <div className={`${styles.contenido} card-content`}>
+                    <h3 className={styles.titulo}>{title}</h3>
+                    <p className={styles.parrafo}>{description}</p>
                 </div>
                 <div className="card-action">
                     <a href={url} target="_blank" rel='noreferrer noopener'
@@ -28,5 +31,9 @@ const Noticia = ({noticia}) => {
         </div>
     );
 };
+
+Noticia.propTypes = {
+    noticia: PropTypes.object.isRequired
+}
 
 export default Noticia;
